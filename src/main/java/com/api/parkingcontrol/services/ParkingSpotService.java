5461ffc4f1573a62970a6d30dc798317ba959fc2
Raí Magalhães Barreira <rmbraibarreira@gmail.com>
2022-12-08 22:52:37 -0300

    revisão de codigo

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,21 +1,18 @@
 package com.api.parkingcontrol.services;
 
-import javax.transaction.Transactional;
-
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import com.api.parkingcontrol.models.ParkingSpotModel;
-import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 
-import java.util.List;
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
-    
-    
+
     final ParkingSpotRepository parkingSpotRepository;
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
@@ -29,17 +26,15 @@ public class ParkingSpotService {
     public boolean existsByLicensePlateCar(String licensePlateCar) {
         return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
     }
-    public boolean existsByParkingSpotNumber(String parkingSpotNumber){
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
         return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
     }
-    public boolean existsByApartmentAndBlock(String apartment, String block){
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
     }
-
     public Page<ParkingSpotModel> findAll(Pageable pageable) {
         return parkingSpotRepository.findAll(pageable);
     }
-
     public Optional<ParkingSpotModel> findById(UUID id) {
         return parkingSpotRepository.findById(id);
     }
